3ebab3c127579e99f94d628b6f13f816b39fe452
Shuvam <shuvam.kumar@groww.in>
2022-08-16 13:48:21 +0530

    Compledted NFT landing website

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="heading-gradient">
+        Made with 🔥 by Shuvam | Developer's Den
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
